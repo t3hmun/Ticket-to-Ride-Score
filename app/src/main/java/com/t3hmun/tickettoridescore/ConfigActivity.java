b7c16dd32b3e717495169c34df20d57e5f69de65
t3hmun <t3hmun@gmail.com>
2017-01-21 13:57:15 +0000

    Empty activity project.
    Launcher icons painted over in orange (for size and obviousness).
    Bigger android gitinore added.

---
+++ app/src/main/java/com/t3hmun/tickettoridescore/ConfigActivity.java
@@ -0,0 +1,13 @@
+package com.t3hmun.tickettoridescore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+}
