c0f03f3298bec015cc3e58b860c889e6979bd1b9
t3hmun <t3hmun@gmail.com>
2017-01-24 10:50:50 +0000

    Removed floating action button imports.

--- app/src/main/java/com/t3hmun/tickettoridescore/MainActivity.java
+++ app/src/main/java/com/t3hmun/tickettoridescore/MainActivity.java
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
